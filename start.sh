4d7747dbf505115f24cdbb00762ca1ac38c389f7
Etchial <98379633+Etchial@users.noreply.github.com>
2023-01-08 14:23:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Etchial/Mdisk-search-Robot /Mdisk-search-Robot
+git clone https://github.com/Etchial/Mdisk-search-Robot .git/Mdisk-search-Robot
 cd /Mdisk-search-Robot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
