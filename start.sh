396478073ab0c24fb7cc2dc9a8bb1cac3a94f4f9
Etchial <98379633+Etchial@users.noreply.github.com>
2023-01-08 13:54:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/Etchial/Mdisk-search-Robot /Mdisk-search-Robot
+cd /Mdisk-search-Robot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
